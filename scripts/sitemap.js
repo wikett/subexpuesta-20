64105609dcf023a45963114e68ed697128369ea5
enriqueaparicio <enrique.ac9@gmail.com>
2020-09-22 09:17:01 +0200

    sitemap generation

---
+++ scripts/sitemap.js
@@ -0,0 +1,86 @@
+'use strict'
+
+const fs = require('fs')
+const xmlParser = require('xml2json')
+const listado = require('../assets/data/localizaciones.json')
+
+function eliminaracento(letra) {
+  switch (letra) {
+    case 'á':
+      return 'a'
+    case 'é':
+      return 'e'
+    case 'í':
+      return 'i'
+    case 'ó':
+      return 'o'
+    case 'ú':
+      return 'u'
+    case 'ñ':
+      return 'n'
+    default:
+      return letra
+  }
+}
+function eliminarcaracteres(text) {
+  let newtext = ''
+  for (const i in text) {
+    newtext += eliminaracento(text[i])
+  }
+  return newtext
+}
+function convertSEO(text) {
+  if (text)
+    return (
+      eliminarcaracteres(text)
+        .replace(/[-\s]+/g, '-') // convert spaces to hyphens
+        .replace(/[^-\w\s]/g, '') // remove unneeded chars
+        .replace(/^\s+|\s+$/g, '') + ''
+    ).toLowerCase() // trim leading/trailing spaces
+}
+
+function formatDate(fechaToma) {
+  const d = new Date(fechaToma)
+  const ye = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(d)
+  const mo = new Intl.DateTimeFormat('en', { month: 'numeric' }).format(d)
+  const da = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(d)
+  return `${ye}-${mo}-${da}`
+}
+
+fs.writeFile(
+  './static/sitemap.xml',
+  '<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">',
+  function (err) {
+    if (err) throw err
+    listado.forEach((item) => {
+      const newUrl = {}
+      newUrl.url = {
+        loc:
+          'https://www.subexpuesta.com/localizaciones/' +
+          item.id +
+          '/' +
+          convertSEO(item.titulo),
+        lastmod: formatDate(item.fechaToma),
+        changefreq: 'monthly',
+        priority: 0.8,
+      }
+      const xml = xmlParser.toXml(newUrl)
+      fs.appendFile('./static/sitemap.xml', xml, function (err) {
+        if (err) {
+          // append failed
+        } else {
+          // done
+          console.log(`${item.titulo}: completado`)
+        }
+      })
+    })
+    fs.appendFile('./static/sitemap.xml', '</urlset>', function (err) {
+      if (err) {
+        // append failed
+      } else {
+        // done
+        console.log(`TODO completado`)
+      }
+    })
+  }
+)
